2f8b04b02ceb684dcad6137fd0a6380f56adc30c
Kleber Mota <klebermo@gmail.com>
2014-11-11 19:26:49 -0300

    Updating fragments

--- src/main/java/org/hello/ArticleFragment.java
+++ src/main/java/org/hello/ArticleFragment.java
@@ -1,8 +1,9 @@
 package org.hello;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class ArticleFragment extends Fragment {

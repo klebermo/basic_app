eea2360658402f63389acea329f4a194ba880874
Kleber Mota <kelbermo@gmail.com>
2014-11-11 17:30:09 -0300

    First commit

---
+++ src/main/java/org/hello/HelloActivity.java
@@ -0,0 +1,56 @@
+package org.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_layout);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Hello world!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.hello_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openSearch() {
+      //
+    }
+
+    public void openSettings() {
+      //
+    }
+
+}

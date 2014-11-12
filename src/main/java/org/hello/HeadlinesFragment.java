58d9f5b6b80416675eceeb74f61c70e18d8d89cd
Kleber Mota <klebermo@gmail.com>
2014-11-12 10:18:46 -0300

    Adding Tabs

--- src/main/java/org/hello/HeadlinesFragment.java
+++ src/main/java/org/hello/HeadlinesFragment.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.FragmentTransaction;
 
 public class HeadlinesFragment extends Fragment {
     @Override
@@ -12,4 +13,80 @@ public class HeadlinesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.headlines_view, container, false);
     }
+
+    /** Called when the user touches the button */
+    public void openTab1(View view) {
+        // Do something in response to button click
+        Tab1Fragment newFragment = new Tab1Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
+    /** Called when the user touches the button */
+    public void openTab2(View view) {
+        // Do something in response to button click
+        Tab2Fragment newFragment = new Tab2Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
+    /** Called when the user touches the button */
+    public void openTab3(View view) {
+        // Do something in response to button click
+        Tab3Fragment newFragment = new Tab3Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
+    /** Called when the user touches the button */
+    public void openTab4(View view) {
+        // Do something in response to button click
+        Tab4Fragment newFragment = new Tab4Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
+    /** Called when the user touches the button */
+    public void openTab5(View view) {
+        // Do something in response to button click
+        Tab5Fragment newFragment = new Tab5Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
 }
---
+++ src/main/java/org/hello/Tab4Fragment.java
@@ -0,0 +1,15 @@
+package org.hello;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab4Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_view, container, false);
+    }
+}
